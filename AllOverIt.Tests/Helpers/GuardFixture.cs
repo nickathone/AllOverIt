537c4789ff4e9abe7b216f1b164ec58275aafe5f
mjfreelancing <malcolm@mjfreelancing.com>
2020-05-14 20:29:06 +1000

    Added Guard helpers +semver: minor
    Includes extension methods as well as expression based helpers

---
+++ AllOverIt.Tests/Helpers/GuardFixture.cs
@@ -0,0 +1,381 @@
+﻿using AllOverIt.Helpers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace AllOverIt.Tests.Helpers
+{
+  public class GuardFixture : AllOverItFixtureBase
+  {
+    private class DummyClass
+    {
+    }
+
+    public class WhenNotNull_Expression : GuardFixture
+    {
+      [Fact]
+      public void Should_Throw_When_Expression_Null()
+      {
+        Invoking(() =>
+          {
+            Guard.WhenNotNull((Expression<Func<DummyClass>>)null);
+          })
+          .Should()
+          .Throw<ArgumentNullException>()
+          .WithMessage(GetExpectedArgumentNullExceptionMessage("expression"));
+      }
+
+      [Fact]
+      public void Should_Throw_When_Null()
+      {
+        Invoking(() =>
+          {
+            DummyClass subject = null;
+
+            Guard.WhenNotNull(() => subject);
+          })
+          .Should()
+          .Throw<ArgumentNullException>()
+          .WithMessage(GetExpectedArgumentNullExceptionMessage("subject"));
+      }
+
+      [Fact]
+      public void Should_Throw_When_Not_MemberExpression()
+      {
+        Invoking(() =>
+          {
+            Guard.WhenNotNull(() => (DummyClass)null);
+          })
+          .Should()
+          .Throw<ArgumentException>()
+          .WithMessage("expression must be a LambdaExpression containing a MemberExpression");
+      }
+
+      [Fact]
+      public void Should_Return_Expression_Value()
+      {
+        Invoking(() =>
+          {
+            var expected = new DummyClass();
+
+            var actual = Guard.WhenNotNull(() => expected);
+
+            actual.Should().BeSameAs(expected);
+          })
+          .Should()
+          .NotThrow();
+      }
+    }
+
+    public class WhenNotNullOrEmpty_Expression_Type : GuardFixture
+    {
+      [Fact]
+      public void Should_Throw_When_Expression_Null()
+      {
+        Invoking(() =>
+          {
+            Guard.WhenNotNullOrEmpty((Expression<Func<IEnumerable<DummyClass>>>)null);
+          })
+          .Should()
+          .Throw<ArgumentNullException>()
+          .WithMessage(GetExpectedArgumentNullExceptionMessage("expression"));
+      }
+
+      [Fact]
+      public void Should_Throw_When_Null()
+      {
+        Invoking(() =>
+          {
+            IEnumerable<DummyClass> subject = null;
+
+            Guard.WhenNotNullOrEmpty(() => subject);
+          })
+          .Should()
+          .Throw<ArgumentNullException>()
+          .WithMessage(GetExpectedArgumentNullExceptionMessage("subject"));
+      }
+
+      [Fact]
+      public void Should_Throw_When_Empty()
+      {
+        var expected = new List<DummyClass>();
+
+        Invoking(() =>
+          {
+            var actual = Guard.WhenNotNullOrEmpty(() => expected);
+
+            actual.Should().BeSameAs(expected);
+          })
+          .Should()
+          .Throw<ArgumentException>()
+          .WithMessage($"The argument cannot be empty (Parameter '{nameof(expected)}')");
+      }
+
+      [Fact]
+      public void Should_Throw_When_Not_MemberExpression()
+      {
+        Invoking(() =>
+          {
+            Guard.WhenNotNullOrEmpty(() => (IEnumerable<DummyClass>)null);
+          })
+          .Should()
+          .Throw<ArgumentException>()
+          .WithMessage("expression must be a LambdaExpression containing a MemberExpression");
+      }
+
+      [Fact]
+      public void Should_Return_Expression_Value()
+      {
+        Invoking(() =>
+          {
+            var expected = new List<DummyClass>
+            {
+              new DummyClass()
+            };
+
+            var actual = Guard.WhenNotNullOrEmpty(() => expected);
+
+            actual.Should().BeSameAs(expected);
+          })
+          .Should()
+          .NotThrow();
+      }
+    }
+
+    public class WhenNotNullOrEmpty_Expression_String : GuardFixture
+    {
+      [Fact]
+      public void Should_Throw_When_Expression_Null()
+      {
+        Invoking(() =>
+          {
+            Guard.WhenNotNullOrEmpty((Expression<Func<string>>)null);
+          })
+          .Should()
+          .Throw<ArgumentNullException>()
+          .WithMessage(GetExpectedArgumentNullExceptionMessage("expression"));
+      }
+
+      [Fact]
+      public void Should_Throw_When_Null()
+      {
+        Invoking(() =>
+          {
+            string subject = null;
+
+            Guard.WhenNotNullOrEmpty(() => subject);
+          })
+          .Should()
+          .Throw<ArgumentNullException>()
+          .WithMessage(GetExpectedArgumentNullExceptionMessage("subject"));
+      }
+
+      [Fact]
+      public void Should_Throw_When_Empty()
+      {
+        var expected = string.Empty;
+
+        Invoking(() =>
+          {
+            var actual = Guard.WhenNotNullOrEmpty(() => expected);
+
+            actual.Should().BeSameAs(expected);
+          })
+          .Should()
+          .Throw<ArgumentException>()
+          .WithMessage($"The argument cannot be empty (Parameter '{nameof(expected)}')");
+      }
+
+      [Fact]
+      public void Should_Throw_When_Not_MemberExpression()
+      {
+        Invoking(() =>
+          {
+            Guard.WhenNotNullOrEmpty(() => (string)null);
+          })
+          .Should()
+          .Throw<ArgumentException>()
+          .WithMessage("expression must be a LambdaExpression containing a MemberExpression");
+      }
+
+      [Fact]
+      public void Should_Return_Expression_Value()
+      {
+        Invoking(() =>
+          {
+            var expected = Create<string>();
+
+            var actual = Guard.WhenNotNullOrEmpty(() => expected);
+
+            actual.Should().Be(expected);
+          })
+          .Should()
+          .NotThrow();
+      }
+    }
+
+    public class WhenNotNull : GuardFixture
+    {
+      [Fact]
+      public void Should_Throw_When_Null()
+      {
+        var name = Create<string>();
+
+        Invoking(() =>
+          {
+            DummyClass dummy = null;
+
+            Guard.WhenNotNull(dummy, name);
+          })
+          .Should()
+          .Throw<ArgumentNullException>()
+          .WithMessage(GetExpectedArgumentNullExceptionMessage($"{name}"));
+      }
+
+      [Fact]
+      public void Should_Not_Throw()
+      {
+        Invoking(() =>
+          {
+            var dummy = new DummyClass();
+
+            Guard.WhenNotNull(dummy, Create<string>());
+          })
+          .Should()
+          .NotThrow();
+      }
+
+      [Fact]
+      public void Should_Return_Object()
+      {
+        var expected = new DummyClass();
+
+        var actual = Guard.WhenNotNull(expected, Create<string>());
+
+        actual.Should().BeSameAs(expected);
+      }
+    }
+
+    public class WhenNotNullOrEmpty_Type : GuardFixture
+    {
+      [Fact]
+      public void Should_Throw_When_Null()
+      {
+        var name = Create<string>();
+
+        Invoking(() =>
+          {
+            IEnumerable<DummyClass> dummy = null;
+
+            Guard.WhenNotNullOrEmpty(dummy, name);
+          })
+          .Should()
+          .Throw<ArgumentNullException>()
+          .WithMessage(GetExpectedArgumentNullExceptionMessage($"{name}"));
+      }
+
+      [Fact]
+      public void Should_Throw_When_Empty()
+      {
+        var name = Create<string>();
+        var expected = new List<DummyClass>();
+
+        Invoking(() =>
+          {
+            Guard.WhenNotNullOrEmpty(expected, name);
+          })
+          .Should()
+          .Throw<ArgumentException>()
+          .WithMessage($"The argument cannot be empty (Parameter '{name}')");
+      }
+
+      [Fact]
+      public void Should_Not_Throw()
+      {
+        Invoking(() =>
+          {
+            var dummy = new List<DummyClass>
+            {
+              new DummyClass()
+            };
+
+            Guard.WhenNotNullOrEmpty(dummy, Create<string>());
+          })
+          .Should()
+          .NotThrow();
+      }
+
+      [Fact]
+      public void Should_Return_Object()
+      {
+        var expected = new List<DummyClass>
+        {
+          new DummyClass()
+        };
+
+        var actual = Guard.WhenNotNullOrEmpty(expected, Create<string>());
+
+        actual.Should().BeSameAs(expected);
+      }
+    }
+
+    public class WhenNotNullOrEmpty_String : GuardFixture
+    {
+      [Fact]
+      public void Should_Throw_When_Null()
+      {
+        var name = Create<string>();
+
+        Invoking(() =>
+          {
+            string dummy = null;
+
+            Guard.WhenNotNullOrEmpty(dummy, name);
+          })
+          .Should()
+          .Throw<ArgumentNullException>()
+          .WithMessage(GetExpectedArgumentNullExceptionMessage($"{name}"));
+      }
+
+      [Fact]
+      public void Should_Throw_When_Empty()
+      {
+        var name = Create<string>();
+        var expected = string.Empty;
+
+        Invoking(() =>
+          {
+            Guard.WhenNotNullOrEmpty(expected, name);
+          })
+          .Should()
+          .Throw<ArgumentException>()
+          .WithMessage($"The argument cannot be empty (Parameter '{name}')");
+      }
+
+      [Fact]
+      public void Should_Not_Throw()
+      {
+        Invoking(() =>
+          {
+            var dummy = Create<string>();
+
+            Guard.WhenNotNullOrEmpty(dummy, Create<string>());
+          })
+          .Should()
+          .NotThrow();
+      }
+
+      [Fact]
+      public void Should_Return_Object()
+      {
+        var expected = Create<string>();
+
+        var actual = Guard.WhenNotNullOrEmpty(expected, Create<string>());
+
+        actual.Should().BeSameAs(expected);
+      }
+    }
+  }
+}
---
+++ AllOverIt/Helpers/Guard.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AllOverIt.Helpers
+{
+  public static class Guard
+  {
+    public static TType WhenNotNull<TType>(Expression<Func<TType>> expression)
+      where TType : class
+    {
+      _ = expression ?? throw new ArgumentNullException(nameof(expression));
+
+      switch (expression)
+      {
+        case LambdaExpression lambdaExpression when lambdaExpression.Body is MemberExpression memberExpression:
+        {
+          var value = expression.Compile().Invoke();
+
+          return value.WhenNotNull(memberExpression.Member.Name);
+        }
+
+        default:
+          throw new ArgumentException($"{nameof(expression)} must be a LambdaExpression containing a MemberExpression");
+      }
+    }
+
+    public static IEnumerable<TType> WhenNotNullOrEmpty<TType>(Expression<Func<IEnumerable<TType>>> expression)
+    {
+      _ = expression ?? throw new ArgumentNullException(nameof(expression));
+
+      switch (expression)
+      {
+        case LambdaExpression lambdaExpression when lambdaExpression.Body is MemberExpression memberExpression:
+        {
+          var value = expression.Compile().Invoke();
+
+          return value.WhenNotNullOrEmpty(memberExpression.Member.Name);
+        }
+
+        default:
+          throw new ArgumentException($"{nameof(expression)} must be a LambdaExpression containing a MemberExpression");
+      }
+    }
+
+    public static string WhenNotNullOrEmpty(Expression<Func<string>> expression)
+    {
+      _ = expression ?? throw new ArgumentNullException(nameof(expression));
+
+      switch (expression)
+      {
+        case LambdaExpression lambdaExpression when lambdaExpression.Body is MemberExpression memberExpression:
+        {
+          var value = expression.Compile().Invoke();
+
+          return value.WhenNotNullOrEmpty(memberExpression.Member.Name);
+        }
+
+        default:
+          throw new ArgumentException($"{nameof(expression)} must be a LambdaExpression containing a MemberExpression");
+      }
+    }
+    
+    #region object extensions
+
+    // returns @object if not null, otherwise throws ArgumentNullException
+    public static TType WhenNotNull<TType>(this TType argument, string name)
+      where TType : class
+    {
+      return argument ?? throw new ArgumentNullException(name);
+    }
+
+    // returns @object if not null or empty, otherwise throws ArgumentNullException / ArgumentException
+    public static IEnumerable<TType> WhenNotNullOrEmpty<TType>(this IEnumerable<TType> argument, string name)
+    {
+      _ = argument ?? throw new ArgumentNullException(name);
+
+      if (!argument.Any())
+      {
+        throw new ArgumentException("The argument cannot be empty", name);
+      }
+
+      return argument;
+    }
+
+    // returns @object if not null or empty, otherwise throws ArgumentNullException / ArgumentException
+    public static string WhenNotNullOrEmpty(this string argument, string name)
+    {
+      _ = argument ?? throw new ArgumentNullException(name);
+
+      if (!string.IsNullOrWhiteSpace(argument))
+      {
+        return argument;
+      }
+
+      throw new ArgumentException("The argument cannot be empty", name);
+    }
+
+    #endregion
+  }
+}
