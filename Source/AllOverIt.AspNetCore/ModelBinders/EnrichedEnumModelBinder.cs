3b628cae953423bcdefcc720eeddfae5b1b2293b
Malcolm Smith <malcolm@mjfreelancing.com>
2021-11-30 14:43:47 +1100

    Updated model binder to cater for quoted query strings (#48)
    * Updated EnrichedEnumModelBinder to remove leading/trailing quotes in case they are provided on a query string
    
    * Removed comment

--- Source/AllOverIt.AspNetCore/ModelBinders/EnrichedEnumModelBinder.cs
+++ Source/AllOverIt.AspNetCore/ModelBinders/EnrichedEnumModelBinder.cs
@@ -35,7 +35,7 @@ namespace AllOverIt.AspNetCore.ModelBinders
 
             TEnum result = null;
 
-            if (enumerationValue == null || EnrichedEnum<TEnum>.TryFromNameOrValue(enumerationName.FirstValue, out result))
+            if (enumerationValue == null || TryGetEnrichedEnum(enumerationName.FirstValue, out result))
             {
                 bindingContext.Result = ModelBindingResult.Success(result);
             }
@@ -47,6 +47,13 @@ namespace AllOverIt.AspNetCore.ModelBinders
             }
 
             return Task.CompletedTask;
+        }
+
+        private static bool TryGetEnrichedEnum(string value, out TEnum result)
+        {
+            // Removing leading/trailing quotes in case they are provided on a query string
+            value = value.Trim('"');
+            return EnrichedEnum<TEnum>.TryFromNameOrValue(value, out result);
         }
     }
 }
