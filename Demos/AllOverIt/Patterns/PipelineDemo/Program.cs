c1929ce0cf9cf4a61a6927c81b7bcf5bc80d6d26
Malcolm Smith <malcolm@mjfreelancing.com>
2022-12-27 16:42:56 +1100

    Pipeline demo updated to use DI

--- Demos/AllOverIt/Patterns/PipelineDemo/Program.cs
+++ Demos/AllOverIt/Patterns/PipelineDemo/Program.cs
@@ -1,41 +1,60 @@
 ﻿using AllOverIt.Patterns.Pipeline;
 using AllOverIt.Patterns.Pipeline.Extensions;
+using Microsoft.Extensions.DependencyInjection;
 using PipelineDemo.Steps;
 using System;
 
 namespace PipelineDemo
 {
+    public delegate string IntegerPipelineProcessor(int value);
+
     internal class Program
     {
         static void Main()
         {
-            var pipeline = PipelineBuilder
+            var services = new ServiceCollection();
 
-               // Using a class type
-               // Only <TPipelineStep, TIn, TOut> because there is no previous input
-               .Pipe<DoubleStep, int, int>()
+            services.AddSingleton<AddFractionStep>();
 
-               // Using a class instance
-               .Pipe(new DoubleStep())
+            // Showing how to register using a IntegerPipelineProcessor delegate
+            services.AddSingleton(provider =>
+            {
+                var addFractionStep = provider.GetRequiredService<AddFractionStep>();
 
-               // TIn is the original input type
-               // TPrevOut is the result type from the previous call
-               // TNextOut is the result type for this next step
-               //
-               // TPipelineStep, TIn, TPrevOut, TNextOut
-               .Pipe<DoubleStep, int, int, int>()
-               
-               // Using a Func
-               .Pipe(value => value * 2)
+                var pipeline = PipelineBuilder
 
-               .Pipe(new AddFractionStep())
+                   // Using a class type
+                   // Only <TPipelineStep, TIn, TOut> because there is no previous input
+                   .Pipe<DoubleStep, int, int>()
 
-               .Pipe(value => $"{value}")
+                   // Using a class instance
+                   .Pipe(new DoubleStep())
 
-               // Build the resulting Func that merges it all together
-               .Build();
+                   // Using a class type (TPipelineStep)
+                   // TPipelineStep, TIn, TPrevOut, TNextOut
+                   //   TIn is the original input type
+                   //   TPrevOut is the result type from the previous call
+                   //   TNextOut is the result type for this next step
+                   .Pipe<DoubleStep, int, int, int>()
 
-            var result = pipeline.Invoke(15);
+                   // Using a Func
+                   .Pipe(value => value * 2)
+
+                   .Pipe(addFractionStep)           // << Much easier this way if using DI
+
+                   .Pipe(value => $"{value}")
+
+                   // Get the resulting Func that has everything merged together
+                   .Build();
+
+                return new IntegerPipelineProcessor(pipeline);
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var pipeline = serviceProvider.GetRequiredService<IntegerPipelineProcessor>();            
+
+            var result = pipeline.Invoke(5);
 
             Console.WriteLine($"{result}");
 
