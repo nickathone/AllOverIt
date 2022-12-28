befe0e6042c064332e8a900e7cc7819eec0a1803
Malcolm Smith <malcolm@mjfreelancing.com>
2022-12-28 11:51:09 +1100

    Adding pipeline tests

--- Source/AllOverIt/Patterns/Pipeline/Extensions/PipelineStepBuilderExtensions.cs
+++ Source/AllOverIt/Patterns/Pipeline/Extensions/PipelineStepBuilderExtensions.cs
@@ -5,19 +5,39 @@ namespace AllOverIt.Patterns.Pipeline.Extensions
 {
     public static class PipelineStepBuilderExtensions
     {
-        // ====== sync
+        /// <summary>Adds a new pipeline step, implemented as a <see cref="Func{TPrevOut, TNextOut}"/>, to an existing pipeline step builder.</summary>
+        /// <typeparam name="TIn">The input type for the first step in the pipeline.</typeparam>
+        /// <typeparam name="TPrevOut">The output type from the previous step in the pipeline.</typeparam>
+        /// <typeparam name="TNextOut">The output type for the step being added to the pipeline.</typeparam>
+        /// <param name="prevStep">The builder instance representing the previous step in the pipeline.</param>
+        /// <param name="step">The step to be added to the pipeline builder.</param>
+        /// <returns>A new pipeline step builder representing the step just added to the pipeline.</returns>
         public static IPipelineStepBuilder<TIn, TNextOut> Pipe<TIn, TPrevOut, TNextOut>(
             this IPipelineStepBuilder<TIn, TPrevOut> prevStep, Func<TPrevOut, TNextOut> step)
         {
             return new PipelineStepBuilder<TIn, TPrevOut, TNextOut>(prevStep, step);
         }
 
+        /// <summary>Adds a new pipeline step, implemented as a <see cref="IPipelineStep{TPrevOut, TNextOut}"/>, to an existing pipeline step builder.</summary>
+        /// <typeparam name="TIn">The input type for the first step in the pipeline.</typeparam>
+        /// <typeparam name="TPrevOut">The output type from the previous step in the pipeline.</typeparam>
+        /// <typeparam name="TNextOut">The output type for the step being added to the pipeline.</typeparam>
+        /// <param name="prevStep">The builder instance representing the previous step in the pipeline.</param>
+        /// <param name="step">The step to be added to the pipeline builder.</param>
+        /// <returns>A new pipeline step builder representing the step just added to the pipeline.</returns>
         public static IPipelineStepBuilder<TIn, TNextOut> Pipe<TIn, TPrevOut, TNextOut>(
             this IPipelineStepBuilder<TIn, TPrevOut> prevStep, IPipelineStep<TPrevOut, TNextOut> step)
         {
             return Pipe(prevStep, step.AsFunc());
         }
 
+        /// <summary>Adds a new pipeline step, implemented as a <see cref="IPipelineStep{TPrevOut, TNextOut}"/>, to an existing pipeline step builder.</summary>
+        /// <typeparam name="TPipelineStep">The pipeline step type, implemented as a <see cref="IPipelineStep{TPrevOut, TNextOut}"/>.</typeparam>
+        /// <typeparam name="TIn">The input type for the first step in the pipeline.</typeparam>
+        /// <typeparam name="TPrevOut">The output type from the previous step in the pipeline.</typeparam>
+        /// <typeparam name="TNextOut">The output type for the step being added to the pipeline.</typeparam>
+        /// <param name="prevStep">The builder instance representing the previous step in the pipeline.</param>
+        /// <returns>A new pipeline step builder representing the step just added to the pipeline.</returns>
         public static IPipelineStepBuilder<TIn, TNextOut> Pipe<TPipelineStep, TIn, TPrevOut, TNextOut>(
             this IPipelineStepBuilder<TIn, TPrevOut> prevStep) where TPipelineStep : IPipelineStep<TPrevOut, TNextOut>, new()
         {
---
+++ Tests/AllOverIt.Tests/Patterns/Pipeline/Extensions/PipelineStepBuilderExtensionsFixture.cs
@@ -0,0 +1,126 @@
+﻿using AllOverIt.Fixture;
+using AllOverIt.Fixture.Extensions;
+using AllOverIt.Patterns.Pipeline;
+using AllOverIt.Patterns.Pipeline.Extensions;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace AllOverIt.Tests.Patterns.ChainOfResponsibility.Extensions
+{
+    public class PipelineStepBuilderExtensionsFixture : FixtureBase
+    {
+        internal class PipelineStepBuilderDummy : IPipelineStepBuilder<int, double>
+        {
+            public Func<int, double> Build()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        internal class StepDummy : IPipelineStep<double, string>
+        {
+            public string Execute(double input)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private readonly PipelineStepBuilderDummy _dummyBuilder = new();
+        private readonly Func<double, string> _funcStep = value => value.ToString();
+        private readonly StepDummy _stepDummy = new();
+
+        public class Pipe_Func : PipelineStepBuilderExtensionsFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Prev_Step_Null()
+            {
+                Invoking(() =>
+                {
+                    PipelineStepBuilderExtensions.Pipe<int, double, string>((IPipelineStepBuilder<int, double>) null, _funcStep);
+                })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("prevStep");
+            }
+
+            [Fact]
+            public void Should_Throw_When_Step_Null()
+            {
+                Invoking(() =>
+                {
+                    PipelineStepBuilderExtensions.Pipe<int, double, string>(_dummyBuilder, (Func<double, string>) null);
+                })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("step");
+            }
+
+            [Fact]
+            public void Should_Create_Step_Builder()
+            {
+                var actual = PipelineStepBuilderExtensions.Pipe<int, double, string>(_dummyBuilder, _funcStep);
+
+                actual.Should().BeOfType<PipelineStepBuilder<int, double, string>>();
+            }
+        }
+
+        public class Pipe_Step_Instance : PipelineStepBuilderExtensionsFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Prev_Step_Null()
+            {
+                Invoking(() =>
+                {
+                    PipelineStepBuilderExtensions.Pipe<int, double, string>((IPipelineStepBuilder<int, double>) null, _stepDummy);
+                })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("prevStep");
+            }
+
+            [Fact]
+            public void Should_Throw_When_Step_Null()
+            {
+                Invoking(() =>
+                {
+                    PipelineStepBuilderExtensions.Pipe<int, double, string>(_dummyBuilder, (IPipelineStep<double, string>) null);
+                })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("step");
+            }
+
+            [Fact]
+            public void Should_Create_Step_Builder()
+            {
+                var actual = PipelineStepBuilderExtensions.Pipe<int, double, string>(_dummyBuilder, _stepDummy);
+
+                actual.Should().BeOfType<PipelineStepBuilder<int, double, string>>();
+            }
+        }
+
+        public class Pipe_Step_Type : PipelineStepBuilderExtensionsFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Prev_Step_Null()
+            {
+                Invoking(() =>
+                {
+                    PipelineStepBuilderExtensions.Pipe<StepDummy, int, double, string>((IPipelineStepBuilder<int, double>) null);
+                })
+                    .Should()
+                    .Throw<ArgumentNullException>()
+                    .WithNamedMessageWhenNull("prevStep");
+            }
+
+            [Fact]
+            public void Should_Create_Step_Builder()
+            {
+                var actual = PipelineStepBuilderExtensions.Pipe<StepDummy, int, double, string>(_dummyBuilder);
+
+                actual.Should().BeOfType<PipelineStepBuilder<int, double, string>>();
+            }
+        }
+    }
+}
