da89a237300cc2114b1dad8a81cb6bd2b8ca9967
Malcolm Smith <malcolm@mjfreelancing.com>
2022-12-29 22:01:20 +1100

    Added more pipeline doc comments

--- Source/AllOverIt/Patterns/Pipeline/IPipelineBuilder.cs
+++ Source/AllOverIt/Patterns/Pipeline/IPipelineBuilder.cs
@@ -2,8 +2,13 @@
 
 namespace AllOverIt.Patterns.Pipeline
 {
+    /// <summary>Represents a synchronous pipeline builder.</summary>
+    /// <typeparam name="TIn">The input type for the pipeline.</typeparam>
+    /// <typeparam name="TOut">The output type for the pipeline.</typeparam>
     public interface IPipelineBuilder<TIn, TOut>
     {
+        /// <summary>Builds the pipeline's steps into a Func that can later be invoked.</summary>
+        /// <returns>The pipeline's steps composed to a single Func that can later be invoked.</returns>
         Func<TIn, TOut> Build();
     }
 }
--- Source/AllOverIt/Patterns/Pipeline/IPipelineBuilderAsync.cs
+++ Source/AllOverIt/Patterns/Pipeline/IPipelineBuilderAsync.cs
@@ -3,8 +3,13 @@ using System.Threading.Tasks;
 
 namespace AllOverIt.Patterns.Pipeline
 {
+    /// <summary>Represents an asynchronous pipeline builder.</summary>
+    /// <typeparam name="TIn">The input type for the pipeline.</typeparam>
+    /// <typeparam name="TOut">The output type for the pipeline.</typeparam>
     public interface IPipelineBuilderAsync<TIn, TOut>
     {
+        /// <summary>Builds the pipeline's steps into a Func that can later be invoked.</summary>
+        /// <returns>The pipeline's steps composed to a single Func that can later be invoked.</returns>
         Func<TIn, Task<TOut>> Build();
     }
 }
