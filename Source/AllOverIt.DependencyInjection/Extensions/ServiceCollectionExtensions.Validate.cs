39b88351fe05e090b5f806e77c900d0c78f8da28
Malcolm Smith <malcolm@mjfreelancing.com>
2023-01-01 16:47:47 +1100

    Added extension method to validate service collection registrations

---
+++ Source/AllOverIt.DependencyInjection/Extensions/ServiceCollectionExtensions.Validate.cs
@@ -0,0 +1,54 @@
+﻿using AllOverIt.Assertion;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace AllOverIt.DependencyInjection.Extensions
+{
+    public static partial class ServiceCollectionExtensions
+    {
+        /// <summary>Validates all registered services to ensure their dependencies are also registered.</summary>
+        /// <param name="services">The service collection to be validated.</param>
+        /// <param name="provider">The service provider built for the collection of registered services.</param>
+        /// <returns>A <see cref="ServiceRegistrationError"/> for each error found.</returns>
+        /// <remarks>This method should only be used during development as it is an expensive operation.</remarks>
+        public static IReadOnlyCollection<ServiceRegistrationError> Validate(this IServiceCollection services, IServiceProvider provider)
+        {
+            _ = services.WhenNotNull(nameof(services));
+            _ = provider.WhenNotNull(nameof(provider));
+
+            var errors = new List<ServiceRegistrationError>();
+
+            foreach (var service in services)
+            {
+                // Skip generic services as we don't know how to deal with them
+                if (service.ServiceType.ContainsGenericParameters)
+                {
+                    continue;
+                }
+
+                ServiceRegistrationError error = null;
+
+                try
+                {
+                    _ = provider.GetRequiredService(service.ServiceType);
+                }
+                catch(Exception exception)
+                {
+                    error = new ServiceRegistrationError
+                    {
+                        Service = service,
+                        Exception = exception
+                    };
+                }
+
+                if (error is not null)
+                {
+                    errors.Add(error);
+                }
+            }
+
+            return errors;
+        }
+    }
+}
---
+++ Tests/AllOverIt.DependencyInjection.Tests/Extensions/ServiceCollectionExtensionsFixture.Validate.cs
@@ -0,0 +1,101 @@
+﻿using AllOverIt.DependencyInjection.Extensions;
+using AllOverIt.Fixture;
+using AllOverIt.Fixture.Extensions;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using Xunit;
+namespace AllOverIt.DependencyInjection.Tests.Extensions
+{
+    public partial class ServiceCollectionExtensionsFixture
+    {
+        public class Validate : ServiceCollectionExtensionsFixture
+        {
+            private class InnerClassDummy
+            {
+            }
+
+            private class ParentClassDummy
+            {
+                public ParentClassDummy(InnerClassDummy inner)
+                {
+                }
+            }
+
+            [Fact]
+            public void Should_Throw_When_Services_Null()
+            {
+                Invoking(() =>
+                {
+                    ServiceCollectionExtensions.Validate(null, A.Fake<IServiceProvider>());
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("services");
+            }
+
+            [Fact]
+            public void Should_Throw_When_Provider_Null()
+            {
+                Invoking(() =>
+                {
+                    ServiceCollectionExtensions.Validate(A.Fake<IServiceCollection>(), null);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("provider");
+            }
+
+            [Fact]
+            public void Should_Not_Throw_When_Services_Empty()
+            {
+                Invoking(() =>
+                {
+                    var services = new ServiceCollection();
+                    var provider = services.BuildServiceProvider();
+
+                    ServiceCollectionExtensions.Validate(services, provider);
+                })
+               .Should()
+               .NotThrow();
+            }
+
+            [Fact]
+            public void Should_Not_Report_Errors_When_All_Dependencies_Registered()
+            {
+                var services = new ServiceCollection();
+
+                services.AddSingleton<InnerClassDummy>();
+                services.AddSingleton<ParentClassDummy>();
+
+                var provider = services.BuildServiceProvider();
+
+                var errors = ServiceCollectionExtensions.Validate(services, provider);
+
+                errors.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void Should_Report_Errors_When_Not_All_Dependencies_Registered()
+            {
+                var services = new ServiceCollection();
+
+                services.AddSingleton<ParentClassDummy>();
+
+                var provider = services.BuildServiceProvider();
+
+                var errors = ServiceCollectionExtensions.Validate(services, provider);
+
+                errors.Should().HaveCount(1);
+
+                var error = errors.First();
+
+                error.Exception.Should().NotBeNull();
+                error.Service.Should().NotBeNull();
+                error.Service.ImplementationType.Should().Be(typeof(ParentClassDummy));
+            }
+        }
+    }
+}
