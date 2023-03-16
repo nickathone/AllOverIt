9d83d7bcbc055d62c85a0337dfbaffd3ba5cad17
Malcolm Smith <malcolm@mjfreelancing.com>
2023-03-16 12:23:23 +1100

    Add service provider extension for named services

---
+++ Source/AllOverIt.DependencyInjection/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AllOverIt.DependencyInjection.Extensions
+{
+    public static partial class ServiceProviderExtensions
+    {
+        public static TService GetRequiredNamedService<TService>(this IServiceProvider provider, string name) where TService : class
+        {
+            return provider
+                .GetRequiredService<INamedServiceResolver<TService>>()
+                .GetRequiredNamedService(name);
+
+            // Could also do:
+            // return provider
+            //     .GetRequiredService<INamedServiceResolver>()
+            //     .GetRequiredNamedService<TService>(name);
+        }
+    }
+}
