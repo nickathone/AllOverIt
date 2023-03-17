c6cfce41cdf177718cfe8960d453b8ce7e93bec4
Malcolm Smith <malcolm@mjfreelancing.com>
2023-03-17 23:50:19 +1100

    Added IComparer extension methods Reverse() and Then()

---
+++ Source/AllOverIt/Extensions/ComparerExtensions.cs
@@ -0,0 +1,74 @@
+﻿using AllOverIt.Assertion;
+using System.Collections.Generic;
+
+namespace AllOverIt.Extensions
+{
+    /// <summary>Provides a variety of extension methods for <see cref="IComparer{TType}"/> types.</summary>
+    public static class ComparerExtensions
+    {
+        // Decorates an IComparer<TType> to negate its result
+        private sealed class ReverseComparer<TType> : IComparer<TType>
+        {
+            private readonly IComparer<TType> _comparer;
+
+            public ReverseComparer(IComparer<TType> comparer)
+            {
+                _comparer = comparer;
+            }
+
+            public int Compare(TType x, TType y)
+            {
+                var result = _comparer.Compare(x, y);
+
+                return result == 0 ? result : -result;
+            }
+        }
+
+        // Applies the chain of responsibility to a pair of IComparer<TType>
+        private sealed class ComparerNode<TType> : IComparer<TType>
+        {
+            private readonly IComparer<TType> _first;
+            private readonly IComparer<TType> _next;
+
+            public ComparerNode(IComparer<TType> first, IComparer<TType> next)
+            {
+                _first = first;
+                _next = next;
+            }
+
+            int IComparer<TType>.Compare(TType x, TType y)
+            {
+                var result = _first.Compare(x, y);
+
+                return result != 0
+                    ? result
+                    : _next.Compare(x, y);
+            }
+        }
+
+        /// <summary>Negates the result of comparing two objects, thereby reversing the order of compared objects.</summary>
+        /// <typeparam name="TType">The object type being compared.</typeparam>
+        /// <param name="comparer">The type comparer.</param>
+        /// <returns>A new comparer that negates the result returned by <paramref name="comparer"/>.</returns>
+        public static IComparer<TType> Reverse<TType>(this IComparer<TType> comparer)
+        {
+            _ = comparer.WhenNotNull(nameof(comparer));
+
+            return new ReverseComparer<TType>(comparer);
+        }
+
+        /// <summary>Creates a comparer that compares an object using <paramref name="first"/>, then by <paramref name="next"/>
+        /// if the first comparer indicated the objects were equal.</summary>
+        /// <typeparam name="TType">The object type being compared.</typeparam>
+        /// <param name="first">The first comparer.</param>
+        /// <param name="next">The next comparer to be invoked if the <paramref name="first"/> indicated two objects were equal.</param>
+        /// <returns>A new comparer that composes <paramref name="first"/> and <paramref name="next"/>.</returns>
+        public static IComparer<TType> Then<TType>(this IComparer<TType> first, IComparer<TType> next)
+        {
+            _ = first.WhenNotNull(nameof(first));
+            _ = next.WhenNotNull(nameof(next));
+
+            return new ComparerNode<TType>(first, next);
+        }
+    }
+}
---
+++ Tests/AllOverIt.Tests/Extensions/ComparerExtensionsFixture.cs
@@ -0,0 +1,116 @@
+﻿using AllOverIt.Extensions;
+using AllOverIt.Fixture;
+using AllOverIt.Fixture.Extensions;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Xunit;
+
+namespace AllOverIt.Tests.Extensions
+{
+    public class ComparerExtensionsFixture : FixtureBase
+    {
+        private sealed class StringDummy
+        {
+            public string First { get; init; }
+            public string Second { get; init; }
+        }
+
+        private sealed class FirstDummyComparer : IComparer<StringDummy>
+        {
+            public static readonly IComparer<StringDummy> Instance = new FirstDummyComparer();
+
+            public int Compare(StringDummy lhs, StringDummy rhs)
+            {
+                return string.Compare(lhs.First, rhs.First);
+            }
+        }
+
+        private sealed class SecondDummyComparer : IComparer<StringDummy>
+        {
+            public static readonly IComparer<StringDummy> Instance = new SecondDummyComparer();
+
+            public int Compare(StringDummy lhs, StringDummy rhs)
+            {
+                return string.Compare(lhs.Second, rhs.Second);
+            }
+        }
+
+        public class Reverse : ComparerExtensionsFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Comparer_Null()
+            {
+                Invoking(() =>
+                {
+                    _ = ComparerExtensions.Reverse<string>(null);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("comparer");
+            }
+
+            [Fact]
+            public void Should_Reverse_Items()
+            {
+                var values = CreateMany<StringDummy>().ToList();
+
+                values.Sort(FirstDummyComparer.Instance.Reverse());
+
+                values.Select(item => item.First).Should().BeInDescendingOrder();
+            }
+        }
+
+        public class Then : ComparerExtensionsFixture
+        {
+            [Fact]
+            public void Should_Throw_When_First_Comparer_Null()
+            {
+                Invoking(() =>
+                {
+                    _ = ComparerExtensions.Then<StringDummy>(null, SecondDummyComparer.Instance);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("first");
+            }
+
+            [Fact]
+            public void Should_Throw_When_Next_Comparer_Null()
+            {
+                Invoking(() =>
+                {
+                    _ = ComparerExtensions.Then<StringDummy>(FirstDummyComparer.Instance, null);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("next");
+            }
+
+            [Fact]
+            public void Should_Compose_Comparers()
+            {
+                var values = new List<StringDummy>
+                {
+                   new StringDummy{ First = "a", Second = Create<string>() },
+                   new StringDummy{ First = "b", Second = Create<string>() },
+                   new StringDummy{ First = "a", Second = Create<string>() },
+                   new StringDummy{ First = "b", Second = Create<string>() },
+                   new StringDummy{ First = "a", Second = Create<string>() },
+                   new StringDummy{ First = "b", Second = Create<string>() },
+                   new StringDummy{ First = "a", Second = Create<string>() },
+                };
+
+                var expected = values.OrderBy(item => item.First).ThenBy(item => item.Second).ToList();
+
+                var sorter = FirstDummyComparer.Instance.Then(SecondDummyComparer.Instance);
+
+                values.Sort(sorter);
+
+                values.Should().ContainInOrder(expected);
+            }
+        }
+    }
+}
