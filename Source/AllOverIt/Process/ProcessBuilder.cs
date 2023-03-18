b12f25b8bfd3b904a3e1c9b1c4482ad5b0536da8
Malcolm Smith <malcolm@mjfreelancing.com>
2023-03-18 13:22:15 +1100

    Add ProcessBuilder tests

--- Source/AllOverIt/Process/ProcessBuilder.cs
+++ Source/AllOverIt/Process/ProcessBuilder.cs
@@ -1,10 +1,19 @@
-﻿namespace AllOverIt.Process
+﻿using AllOverIt.Assertion;
+
+namespace AllOverIt.Process
 {
+    /// <summary>A builder for configuring a <see cref="ProcessExecutor"/> that can be later executed.</summary>
     public static class ProcessBuilder
     {
-        public static ProcessExecutorOptions For(string processFilename)
+        /// <summary>Creates a default <see cref="ProcessExecutorOptions"/> for the provided process filename.</summary>
+        /// <param name="processFileName">The filename for the process to be executed. This should include a fully
+        /// qualified file path if the executable cannot be found on the system path.</param>
+        /// <returns>A default <see cref="ProcessExecutorOptions"/> for the provided process filename.</returns>
+        public static ProcessExecutorOptions For(string processFileName)
         {
-            return new ProcessExecutorOptions(processFilename);
+            _ = processFileName.WhenNotNullOrEmpty(nameof(processFileName));
+
+            return new ProcessExecutorOptions(processFileName);
         }
     }
 }
---
+++ Tests/AllOverIt.Tests/Process/ProcessBuilderFixture.cs
@@ -0,0 +1,71 @@
+﻿using AllOverIt.Fixture;
+using AllOverIt.Fixture.Extensions;
+using AllOverIt.Process;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xunit;
+
+namespace AllOverIt.Tests.Process
+{
+    public class ProcessBuilderFixture : FixtureBase
+    {
+        [Fact]
+        public void Should_Throw_When_Process_FileName_Is_Null()
+        {
+            Invoking(() =>
+            {
+                _ = ProcessBuilder.For(null);
+            })
+            .Should()
+            .Throw<ArgumentNullException>()
+            .WithNamedMessageWhenNull("processFileName");
+        }
+
+        [Fact]
+        public void Should_Throw_When_Process_FileName_Is_Empty()
+        {
+            Invoking(() =>
+            {
+                _ = ProcessBuilder.For(string.Empty);
+            })
+            .Should()
+            .Throw<ArgumentException>()
+            .WithNamedMessageWhenEmpty("processFileName");
+        }
+
+        [Fact]
+        public void Should_Throw_When_Process_FileName_Is_Whitespace()
+        {
+            Invoking(() =>
+            {
+                _ = ProcessBuilder.For("  ");
+            })
+            .Should()
+            .Throw<ArgumentException>()
+            .WithNamedMessageWhenEmpty("processFileName");
+        }
+
+        [Fact]
+        public void Should_Return_Expected_ProcessExecutorOptions()
+        {
+            var processFileName = Create<string>();
+
+            var actual = ProcessBuilder.For(processFileName);
+
+            var expected = new
+            {
+                ProcessFileName = processFileName,
+                WorkingDirectory = (string) default,
+                Arguments = (string) default,
+                Timeout = (TimeSpan) default,
+                EnvironmentVariables = (IDictionary<string, string>) default,
+                StandardOutputHandler = (DataReceivedEventHandler) default,
+                ErrorOutputHandler = (DataReceivedEventHandler) default
+            };
+
+            expected.Should().BeEquivalentTo(actual);
+        }
+    }
+}
