776b1ef80b855d59dc07f2a34fa49bb423ec183e
Malcolm Smith <malcolm@mjfreelancing.com>
2023-04-03 09:23:24 +1000

    Add tests

---
+++ Tests/AllOverIt.Validation.Tests/Validators/ExclusiveBetweenContextValidatorFixture.cs
@@ -0,0 +1,26 @@
+﻿using AllOverIt.Fixture;
+using AllOverIt.Validation.Validators;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace AllOverIt.Validation.Tests.Validators
+{
+    public class ExclusiveBetweenContextValidatorFixture : FixtureBase
+    {
+        [Fact]
+        public void Should_Have_Expected_Name()
+        {
+            var validator = new ExclusiveBetweenContextValidator<int, int, int>(_ => _, _ => _);
+
+            var typeName = typeof(ExclusiveBetweenContextValidator<,,>).Name;
+            var tickIndex = typeName.IndexOf("`", StringComparison.Ordinal);
+
+            tickIndex.Should().BeGreaterThan(-1);
+
+            validator.Name
+                .Should()
+                .Be(typeName[..tickIndex]);
+        }
+    }
+}
---
+++ Tests/AllOverIt.Validation.Tests/Validators/InclusiveBetweenContextValidatorFixture.cs
@@ -0,0 +1,26 @@
+﻿using AllOverIt.Fixture;
+using AllOverIt.Validation.Validators;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace AllOverIt.Validation.Tests.Validators
+{
+    public class InclusiveBetweenContextValidatorFixture : FixtureBase
+    {
+        [Fact]
+        public void Should_Have_Expected_Name()
+        {
+            var validator = new InclusiveBetweenContextValidator<int, int, int>(_ => _, _ => _);
+
+            var typeName = typeof(InclusiveBetweenContextValidator<,,>).Name;
+            var tickIndex = typeName.IndexOf("`", StringComparison.Ordinal);
+
+            tickIndex.Should().BeGreaterThan(-1);
+
+            validator.Name
+                .Should()
+                .Be(typeName[..tickIndex]);
+        }
+    }
+}
