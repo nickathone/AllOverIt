1fd194cdb37157d6978ac168b521f9845cb4c170
Malcolm Smith <malcolm@mjfreelancing.com>
2023-06-15 22:07:23 +1000

    Added AsyncDisposableExtensions tests

---
+++ Source/AllOverIt/Extensions/AsyncDisposableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using AllOverIt.Assertion;
+using AllOverIt.Async;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllOverIt.Extensions
+{
+    /// <summary>Provides a variety of extension methods for <see cref="IAsyncDisposable"/>.</summary>
+    public static class AsyncDisposableExtensions
+    {
+        /// <summary>Asynchronously disposes a collection of <see cref="IAsyncDisposable"/> instances.</summary>
+        /// <param name="disposables">The collection of disposables to asynchronously dispose of.</param>
+        /// <returns>A <see cref="ValueTask"/> that completes when all disposables have been disposed of.</returns>
+        public static ValueTask DisposeAllAsync(this IEnumerable<IAsyncDisposable> disposables)
+        {
+            _ = disposables.WhenNotNull(nameof(disposables));
+
+            var disposableConnections = new CompositeAsyncDisposable(disposables.ToArray());
+            
+            return disposableConnections.DisposeAsync();
+        }
+    }
+}
---
+++ Tests/AllOverIt.Tests/Extensions/AsyncDisposableExtensionsFixture.cs
@@ -0,0 +1,46 @@
+﻿using AllOverIt.Extensions;
+using AllOverIt.Fixture;
+using AllOverIt.Fixture.Extensions;
+using AllOverIt.Patterns.ResourceInitialization;
+using FluentAssertions;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AllOverIt.Tests.Extensions
+{
+    public class AsyncDisposableExtensionsFixture : FixtureBase
+    {
+        public class DisposeAllAsync : AsyncDisposableExtensionsFixture
+        {
+            [Fact]
+            public void Should_Throw_When_Null()
+            {
+                Invoking(() =>
+                {
+                    _ = AsyncDisposableExtensions.DisposeAllAsync(null);
+                })
+                .Should()
+                .Throw<ArgumentNullException>()
+                .WithNamedMessageWhenNull("disposables");
+            }
+
+            [Fact]
+            public async Task Should_Dispose_All_Disposables()
+            {
+                var expected = CreateMany<int>(3);
+                var actual = new[] { 0, 0, 0 };
+
+                var disposable1 = new RaiiAsync(() => { }, () => { actual[0] = expected[0]; return Task.CompletedTask; });
+                var disposable2 = new RaiiAsync(() => { }, () => { actual[1] = expected[1]; return Task.CompletedTask; });
+                var disposable3 = new RaiiAsync(() => { }, () => { actual[2] = expected[2]; return Task.CompletedTask; });
+
+                var disposables = new[] { disposable1, disposable2, disposable3 };
+
+                await AsyncDisposableExtensions.DisposeAllAsync(disposables);
+
+                actual.Should().BeEquivalentTo(expected);
+            }
+        }
+    }
+}
