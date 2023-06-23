74098713a8428228d9b2af0d1c51fd348d53e82e
Malcolm Smith <malcolm@mjfreelancing.com>
2023-06-23 23:08:06 +1000

    Cleanup

--- Source/AllOverIt.Pagination/Extensions/QueryPaginatorExtensions.cs
+++ Source/AllOverIt.Pagination/Extensions/QueryPaginatorExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
-using System.Threading;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace AllOverIt.Pagination.Extensions
 {
