d2023ff72054d6f64e79d6bbe5e60bde9e96b0d4
Malcolm Smith <malcolm@mjfreelancing.com>
2023-06-26 21:48:09 +1000

    Add tests

---
+++ Tests/AllOverIt.Pipes.Tests/PipeExceptionFixture.cs
@@ -0,0 +1,27 @@
+﻿using AllOverIt.Fixture;
+using AllOverIt.Pipes.Exceptions;
+using Xunit;
+
+namespace AllOverIt.Pipes.Tests
+{
+    public class PipeExceptionFixture : FixtureBase
+    {
+        [Fact]
+        public void Should_Have_Default_Constructor()
+        {
+            AssertDefaultConstructor<PipeException>();
+        }
+
+        [Fact]
+        public void Should_Have_Constructor_With_Message()
+        {
+            AssertConstructorWithMessage<PipeException>();
+        }
+
+        [Fact]
+        public void Should_Have_Constructor_With_Message_And_InnerException()
+        {
+            AssertConstructorWithMessageAndInnerException<PipeException>();
+        }
+    }
+}
