ad00853ddc38929a91b35090af8806f82e8751e3
Malcolm Smith <malcolm@mjfreelancing.com>
2023-06-28 15:43:34 +1000

    Blank test class

---
+++ Tests/AllOverIt.Pipes.Tests/NamedPipeFixture_Functional.cs
@@ -0,0 +1,17 @@
+﻿using AllOverIt.Fixture;
+using System;
+
+namespace AllOverIt.Pipes.Tests
+{
+    public class NamedPipeFixture_Functional : FixtureBase
+    {
+        private class DummyMessage
+        {
+            public int Id { get; set; }
+            public string Value { get; set; }
+            public Guid Guid { get; set; }
+        }
+
+
+    }
+}
